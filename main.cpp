6c0d2b29be98fa39138c33b54e259dbcb935d272
Laith <laithsaqqa@gmail.com>
2024-08-25 22:16:07 +0300

    Empty the main

--- main.cpp
+++ main.cpp
@@ -25,70 +25,13 @@ typedef pair<int, int> ii;
 #define oo 1000000007
 const int MOD = 1e9 + 7;
 
-
-// Fenwick Tree
-// 0-based indexing
-template <typename T>
-class Fenwick{
-        vector<T> bit;
-        int n;
-    public:
-        Fenwick(int n){
-            this->n = n;
-            bit.clear();
-            bit.resize(n);
-        }
-        void update(int idx, T val){
-            for(; idx < n; idx |= idx + 1){
-                bit[idx] += val;
-            }
-        }
-        T query(int idx){
-            T res = 0;
-            for(; idx >= 0; idx = (idx & (idx + 1)) - 1){
-                res += bit[idx];
-            }
-            return res;
-        }
-        T query(int l, int r){
-            return query(r) - query(l-1);
-        }
-};
-
-
 void solve(){
-    int n,t,d;
-    cin>>n>>t>>d;
-    Fenwick<int> f(n);
-    for(int i=0;i<n;i++){
-        int s;
-        cin>>s;
-        f.update(i,s);
-    }
-    vector<ii> queries;
-    for(int i=0;i<t;i++){
-        int a,b;
-        cin>>a>>b;
-        a--; b--;
-        queries.pb({a,b});
-    }
-    for(int i=0; i<d; i++){
-        cout<<"Day "<<i+1<<": ";
-        int x; cin>>x;
-        for(int j=0;j<x;j++){
-            int k; cin>>k; k--;
-            f.update(k,1);
-        }
-        for(auto q: queries){
-            cout<<f.query(q.f,q.s)<<" ";
-        }
-        cout<<endl;
-    }
+    // write your code here
 }
 
 int main(){
     int t=1;
-    // scanf("%d",&t);
+    scanf("%d",&t);
     
     int tt = t;
     while(t--){
